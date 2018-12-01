c6499deca10d7eca67bd21b0526871400a97f820
JorellanaF <00103717@uca.edu.sv>
2018-12-01 14:07:37 -0600

    Estructura express y modelo user

---
+++ API/models/user.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+
+const UserSchema = Schema({
+    materia: String,
+    uv: String,
+    descripcion: String
+})
+
+module.exports = user
